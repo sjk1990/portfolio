1e4da24e5e6d90c354e58cd6604ebeddef328fde
sjk1990 <worldsjk1990@gmail.com>
2021-01-16 21:49:11 +0900

    JS navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//navbar 메뉴 클릭시 해당 화면으로 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  //   console.dir(event.target.dataset.link);
+  const link = event.target.dataset.link;
+  if (!link) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  console.log(scrollTo);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
