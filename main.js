8c141d2860c389b56a93bf0d3bb1fdaaea442e7a
sjk1990 <worldsjk1990@gmail.com>
2021-01-16 18:44:27 +0900

    JS Navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 스크롤시 navbar 배경색을 추가함
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
